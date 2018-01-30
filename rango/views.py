0124803feddf3dabc3ae94b6753ab2115c07933d
Jak Scott <2354987s@student.gla.ac.uk>
2018-01-30 18:49:40 +0000

    Chapter 8 plus exercises

--- rango/views.py
+++ rango/views.py
@@ -42,7 +42,6 @@ def add_page(request, category_name_slug):
                 category = Category.objects.get(slug=category_name_slug)
         except Category.DoesNotExist:
                 category = None
-                
         form = PageForm()
         if request.method == 'POST':
                 form = PageForm(request.POST)
@@ -54,10 +53,8 @@ def add_page(request, category_name_slug):
                                 page.save()
                         return show_category(request, category_name_slug)
                 else:
-                        print(form.errors)
-                        
-        context_dict = {'form': form, 'category': category}
-        
+                        print(form.errors)  
+        context_dict = {'form': form, 'category': category}  
         return render(request, 'rango/add_page.html', context_dict)
 
 
